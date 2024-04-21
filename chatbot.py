5bb8615e795c6fa3ae4f9c2a497b4f0c5e1b8e6e
derjo1981 <Johannes.hintermaier@gmx.de>
2024-04-21 17:04:45 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,12 +7,8 @@ from openai import OpenAI
 import tiktoken
 import json
 from datetime import datetime
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get('API_KEY')
-
+api_key = st.secrets.API_KEY
 
 #creating ConversationManager class 
 
